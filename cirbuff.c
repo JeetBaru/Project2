ad1c00bccff70c0440d537af072615194c52bf24
Sudeep Kulkarni <suku8854@colorado.edu>
2017-03-08 00:28:51 -0700

    adding cirbuff.c

--- cirbuff.c
+++ cirbuff.c
@@ -2,39 +2,52 @@
 # include <stdlib.h>
 #include "cirbuff.h"
 
-
-int8_t is_buffer_full(circ_buff * cb1)
+int8_t CircBuf_size(circ_buff*cb1)
 {
-	if(cb1->count == cb1->size)
+    cb1= malloc(sizeof(circ_buff));
+    return SUCCESS;
+
+}
+
+int8_t is_buffer_full(circ_buff * cb1, uint8_t count, uint8_t size)
+{	//overfill
+	
+	cb1->count = count;
+	cb1->size = size;
+	if(cb1->count == cb1->size )
 		return FULL;
 	else
 		return NO;
 }
 
-int8_t is_buffer_empty(circ_buff * cb1)
-{
+int8_t is_buffer_empty(circ_buff * cb1, uint8_t count)
+{	
+	//overempty
+	if (!cb1)
+		return INVALID_POINTER;
+	cb1->count = count;
 	if(cb1->count == 0)
 		return EMPTY;
 	else
 		return NO;
 }
 
-void initialize_buffer(circ_buff * cb1, int8_t size)
+int8_t initialize_buffer(circ_buff * cb1, int8_t size)
 {
 	cb1->buff = (int8_t *)calloc(size,(sizeof(int8_t)));
 	cb1->head = cb1->buff;
 	cb1->tail = cb1->buff;
 	cb1->size = size;
 	cb1->count = 0;
+	return SUCCESS;
 }
 
 void destroy_buffer(circ_buff * cb1)
 {
 	free(cb1->buff);
-	cb1->buff == '\0';
 }
 
-void add_item(circ_buff * cb1, int8_t data)
+int8_t add_item(circ_buff * cb1, int8_t data)
 {
 	if(cb1){
 		if(cb1->head==cb1->buff+cb1->size-1){
@@ -47,10 +60,12 @@ void add_item(circ_buff * cb1, int8_t data)
 		}
 	}
 	cb1->count++;
+   	return WRAP_AROUND_SUCCESS;
 }
 
-uint8_t remove_item(circ_buff * cb1)
+uint8_t remove_item(circ_buff * cb1, uint8_t size)
 {
+	
 	if(cb1){
 		if(cb1->tail==cb1->buff+cb1->size-1){
 			cb1->tail=cb1->buff;
@@ -59,6 +74,8 @@ uint8_t remove_item(circ_buff * cb1)
 			cb1->tail++;
 		}
 		cb1->count--;
-		return *(cb1->tail);
 	}
+	return WRAP_AROUND_SUCCESS;
+	
 }
+
