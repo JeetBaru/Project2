d1c3f923cc527ca1181842b8d9ee45751c9c4ea4
Sudeep Kulkarni <suku8854@colorado.edu>
2017-03-12 13:44:25 -0600

    adding memory.c

--- memory.c
+++ memory.c
@@ -1,21 +1,30 @@
-# include "memory.h"
-# include <stdint.h>
-#include "cirbuff.h"
+/******************************************************
+*   File: memory.c
+*
+*   Copyrights 2016  Sudeep Kulkarniirlekar and Jeet Baru
+*   All Rights Reserved
+*
+*
+*   The information contained herein is property of the Authors. 
+*   The copying and distribution of the files is prohibited except
+*   by express written agreement with the Authors.
+*
+*
+*   Authors: Sudeep Kulkarniirlekar and Jeet BAru
+*   Date Edited: 12 March 2017
+*
+*   Description: Source file for implementing Circular buffer functions
+*              - my_memmove
+*              - my_memset
+*	       - my_memzero
+*              - my_rev
+*              
+********************************************************/
+#include "memory.h"
+#include "uni.h"
 
-/*typedef enum retData{
-	NULL_PTR,
-	SUCCESS
-	};*/
-
-/*////////////////////////////////////////////////////////////////////////
-//Name	   : my_memmov													//
-//Params   : source pointer, destination pointer, length				//
-//Function : This function moves data of specified length 				//	
-//			 from source location to destination. The on moving 		//
-//			 to the destination the data at source is made zero.		//
-//Returns  : 0 for error, 1 for successful operation					//
-////////////////////////////////////////////////////////////////////////*/	
-
+	
+//This function moves data of specified length from source location to destination. The on moving to the destination the data at source is made zero.
 int8_t my_memmov(uint8_t * scr, uint8_t * dest,uint32_t length)
 {
 	int8_t i;
@@ -42,15 +51,8 @@ int8_t my_memmov(uint8_t * scr, uint8_t * dest,uint32_t length)
 	return SUCCESS;
 }
 
-//////////////////////////////////////////////////////////////////////////
-//Name	   : my_memset													//
-//Params   : source pointer, data, length								//
-//Function : This function copies the specified value of data onto 		//	
-//			 the memory location pointed by the source pointer for 		//
-//			 length l. The data in memory from address of source 		//
-//			 pointer till specified length is overwritten by value.		// 
-//Returns  : 0 for error, 1 for successful operation					//
-////////////////////////////////////////////////////////////////////////*/
+//copies the specified value of data onto the memory location pointed by the source pointer for length l. The data in memory from 
+//address of source pointer till specified length is overwritten by value.
 
 int8_t my_memset(uint8_t * scr, uint32_t length,uint8_t value)
 {
@@ -66,16 +68,9 @@ int8_t my_memset(uint8_t * scr, uint32_t length,uint8_t value)
 	return SUCCESS;	
 }
 
-/*////////////////////////////////////////////////////////////////////////
-//Name	   : my_memzero													//
-//Params   : source pointer, length										//
-//Function : This function copies 0 onto the memory location pointed	//	
-//			 by the source pointer for given length. The data in memory	//
-//			 from address of source pointer till specified length		//
-//			 is overwritten by 0.										// 
-//Returns  : 0 for error, 1 for successful operation					//
-////////////////////////////////////////////////////////////////////////*/
 
+//copies 0 onto the memory location pointed by the source pointer for given length. The data in memory from address of source 
+ //pointer till specified length is overwritten by 0.
 int8_t my_memzero(uint8_t *scr, uint32_t length)
 {
 	int8_t i;
@@ -90,14 +85,8 @@ int8_t my_memzero(uint8_t *scr, uint32_t length)
 	return SUCCESS;
 }
 
-/*////////////////////////////////////////////////////////////////////////
-//Name	   : my_rev														//
-//Params   : source pointer, length										//
-//Function : This function reverses an array of all elements in   		//	
-//			 the memorylocation starting at address pointed by  		//
-//			 the source pointer till length specified.  				// 
-//Returns  : 0 for error, 1 for successful operation					//
-////////////////////////////////////////////////////////////////////////*/
+//This function reverses an array of all elements inthe memorylocation starting at address pointed by  
+//the source pointer till length specified.
 
 int8_t my_rev(uint8_t * ptr,uint32_t length)
 {
