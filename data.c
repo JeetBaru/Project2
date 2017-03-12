ee168b2351583585c021570880d42a68f0dcce1d
Sudeep Kulkarni <suku8854@colorado.edu>
2017-03-12 13:42:35 -0600

    adding data.c

--- data.c
+++ data.c
@@ -1,16 +1,32 @@
+/******************************************************
+*   File: data.c
+*
+*   Copyrights 2016  Sudeep Kulkarniirlekar and Jeet Baru
+*   All Rights Reserved
+*
+*
+*   The information contained herein is property of the Authors. 
+*   The copying and distribution of the files is prohibited except
+*   by express written agreement with the Authors.
+*
+*
+*   Authors: Sudeep Kulkarniirlekar and Jeet BAru
+*   Date Edited: 12 March 2017
+*
+*   Description: Source file for implementing Circular buffer functions
+*              - my_itoa
+*              - my_atoi
+*	       - big_to_little32
+*              - little_to_big32
+*              - print_memory
+*              
+********************************************************/
 #include "data.h"
-#include "cirbuff.h"
-#include <stdint.h>
+#include "uni.h"
 
 
-/*////////////////////////////////////////////////////////////////////////
-//Name	   : my_itoa													//
-//Params   : destination pointer,data ,base								//
-//Function : This function converts the given data into its equivalent  //	
-//			 with the specified base and store the result as a string  	//
-//		 	 in the memory location starting from destination pointer	// 
-//Returns  : pointer to start of ascii string, or 0 for error			//
-////////////////////////////////////////////////////////////////////////*/
+//This function converts the given data into its equivalent with the specified base and store the result as a string  	
+//in the memory location starting from destination pointer
 
 int8_t * my_itoa(int8_t * str, int32_t data, int32_t base)
 {
@@ -63,16 +79,9 @@ int8_t * my_itoa(int8_t * str, int32_t data, int32_t base)
 	return str;
 }
 
-/*////////////////////////////////////////////////////////////////////////
-//Name	   : my_atoi													//
-//Params   : source pointer												//
-//Function : This function converts the given ascii string 		 		//	
-//			 starting from memory location pointed at by source   		//
-//		 	 pointer to integer data value of base 10 provided 			//
-//			 all characters are digits.									//
-//Returns  : 32 bit data post conversion, or 0 for error				//
-////////////////////////////////////////////////////////////////////////*/
-
+//This function converts the given ascii string starting from memory location pointed at by source   		
+//pointer to integer data value of base 10 provided 		
+//all characters are digits.
 int32_t my_atoi(int8_t * str)
 {
 	int32_t data=0; 													//initializing data and sign
@@ -122,21 +131,14 @@ int32_t my_atoi(int8_t * str)
 				return 0;
 			}
 		}
-		return sign*data;												//return data with sign
+		return sign*data;										//return data with sign
 	}
 }
 
-/*////////////////////////////////////////////////////////////////////////
-//Name	   : big_to_little32											//
-//Params   : 32 bit pointer data, length								//
-//Function : This function converts the given 32 bit data stored in  	//	
-//			 big endian form starting from memory location pointed   	//
-//		 	 at by data pointer to its little endian form. This 		//
-//			 continues for provided length. length is number 			//
-//			 of such 32 bit data entries								//
-//Returns  : 0 for error, 1 for successful operation					//
-////////////////////////////////////////////////////////////////////////*/
 
+//This function converts the given 32 bit data stored in big endian form starting from memory location pointed  
+//at by data pointer to its little endian form. This continues for provided length. length is number 	
+//of such 32 bit data entries
 int8_t big_to_little32(uint32_t * data,uint32_t length)
 {
 	uint8_t * p;
@@ -163,16 +165,9 @@ int8_t big_to_little32(uint32_t * data,uint32_t length)
 	return SUCCESS;
 }
 
-/*////////////////////////////////////////////////////////////////////////
-//Name	   : little_to_big32											//
-//Params   : 32 bit pointer data, length								//
-//Function : This function converts the given 32 bit data stored in  	//	
-//			 little endian form starting from memory location pointed  	//
-//		 	 at by data pointer to its big endian form. This 			//
-//			 continues for provided length. length is number 			//
-//			 of such 32 bit data entries								//
-//Returns  : 0 for error, 1 for successful operation					//
-////////////////////////////////////////////////////////////////////////*/
+//This function converts the given 32 bit data stored in little endian form starting from memory location pointed  	
+//at by data pointer to its big endian form. This continues for provided length. length is number 		
+//of such 32 bit data entries
 
 int8_t little_to_big32(uint32_t * data,uint32_t length)
 {
@@ -200,14 +195,8 @@ int8_t little_to_big32(uint32_t * data,uint32_t length)
 	return SUCCESS;
 }
 
-/*////////////////////////////////////////////////////////////////////////
-//Name	   : print_memory												//
-//Params   : pointer start, length										//
-//Function : This function prints contents of memory starting from  	//	
-//			 memory location pointed at by start till the specified 	//
-//		 	 length. It prints the hex equivalent of the data stored 	//
-//Returns  : void return type											//
-////////////////////////////////////////////////////////////////////////*/
+//This function prints contents of memory starting from memory location pointed at by start till the specified 
+//length. It prints the hex equivalent of the data stored
 
 void print_memory(uint8_t * start, uint32_t length)
 {
