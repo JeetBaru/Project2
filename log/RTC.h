21286882d6edc3817c421b06e034f074d7e73791
Sudeep Kulkarni <suku8854@colorado.edu>
2017-04-16 21:12:13 -0600

    Adding RTC file

--- log/RTC.h
+++ log/RTC.h
@@ -7,8 +7,21 @@
 
 #ifndef SOURCES_RTC_H_
 #define SOURCES_RTC_H_
-
+/*****************************************************************************************************
+*rtc_init
+*	description: This function is used to configure the RTC module
+*	parameters:
+		This function has no parameters
+*   return : This function does not return anything
+*****************************************************************************************************/
 void rtc_init(void);
+/*****************************************************************************************************
+*GPIO_Init
+*	description:This function is used to lock the current value of RTC_TSR
+*	parameters:
+		This function has no parameters
+*   return : Returns the value of RTC_TSR
+*****************************************************************************************************/
 uint32_t get_time();
 
 #endif /* SOURCES_RTC_H_ */
